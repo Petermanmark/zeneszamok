53dc33cd7066a3f232dee4e0f987edb3736f1319
Márk <petermanmatk12@gmail.com>
2022-09-28 11:22:46 +0200

    minden is

---
+++ index.js
@@ -0,0 +1,20 @@
+let zeneCime = document.getElementById('zenecime');
+let zeneHossza = document.getElementById('zenehossz');
+let zeneAdd = document.getElementById('zeneadd');
+let eredmeny = document.getElementById('eredmeny')
+
+let cim = "AMOGS";
+let hossz = 15;
+
+class Zene {
+    zeneCime = "";
+    zeneHossza = 0;
+    static list = [];
+
+    constructor(zeneCime, zeneHossza){
+        
+    }
+}
+
+Zene.list.push(cim, hossz);
+console.log(Zene.list);
